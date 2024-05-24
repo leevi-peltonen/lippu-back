74ebe604b545efc8e05d8939a8b21753d994907c
leevi-peltonen <leevi.peltonen@icloud.com>
2024-05-24 12:33:47 +0300

    testing

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: 'https://courageous-bubblegum-433632.netlify.app/',
+        origin: 'https://courageous-bubblegum-433632.netlify.app',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
