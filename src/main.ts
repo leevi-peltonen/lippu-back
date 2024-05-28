4a33bc7fbffbdda2038d48fef213648c23e99cde
leevi-peltonen <leevi.peltonen@icloud.com>
2024-05-28 13:31:30 +0300

    fix main connection

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
-
+const port = process.env.PORT || 3000;
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new IoAdapter(app))
-
-    await app.listen(3000);
+    
+    await app.listen(port, "0.0.0.0");
 }
 bootstrap();
