7ccdd443c03b921cea8b587efc645d5dc8c2e309
leevi-peltonen <leevi.peltonen@icloud.com>
2024-05-28 13:12:29 +0300

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new IoAdapter(app))
-    app.enableCors({
-        origin: 'https://6655a9fa76e36d0008e42999--courageous-bubblegum-433632.netlify.app',
-        methods: ['GET', 'POST'],
-        credentials: true,
-    });
 
     await app.listen(3000);
 }
